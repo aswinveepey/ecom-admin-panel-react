39cf7e2ce9fe47fd821d7f8969689ab2ffd45e54
LIT-Admin <aswin@littech.in>
2020-06-12 12:49:05 +0530

    css search

--- src/components/common/appbar.js
+++ src/components/common/appbar.js
@@ -4,14 +4,71 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
+import InputBase from "@material-ui/core/InputBase";
+// import Grid from "@material-ui/core/Grid";
 //Material Icon Imports
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
+//styles import
+import { fade, withStyles } from "@material-ui/core/styles";
 //Relative imports
 import DrawerComp from "./drawer"; //sidebar drawer
 
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderBottom: '2px solid black',
+    borderRadius: theme.shape.borderRadius,
+    // backgroundColor: fade(theme.palette.common.white, 0.15),
+    // "&:hover": {
+    //   backgroundColor: fade(theme.palette.common.white, 0.25),
+    // },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm" | "md" | "xs")]: {
+      width: "25ch",
+      "&:focus": {
+        width: "125ch",
+      },
+    },
+  },
+});
 class AppBarComp extends React.Component {
   state = { open: false };
   handleDrawerToggle = () => {
@@ -26,46 +83,37 @@ class AppBarComp extends React.Component {
     document.addEventListener("keydown", this.escFunction, false);
   }
   render() {
+    const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.root}>
         {/* <CssBaseline /> */}
-        <AppBar position="static" className="appbar" >
-          <Toolbar className='app-toolbar'>
-            <Grid container>
-              <Grid item lg={4}>
-                <Grid container>
-                  <Grid item>
-                    <IconButton
-                      color="inherit"
-                      aria-label="open drawer"
-                      onClick={this.handleDrawerToggle}
-                      edge="start"
-                    >
-                      <MenuIcon />
-                    </IconButton>
-                  </Grid>
-                  <Grid item style={{ padding: "8px" }}>
-                    <Typography variant="h6" noWrap>
-                      {this.props.title}
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item lg={6}>
-                <Grid container>
-                  <Grid item style={{ padding: "8px" }}>
-                    <SearchIcon />
-                  </Grid>
-                  <Grid item>
-                    <TextField
-                      type="outlined"
-                      placeholder="Search..."
-                      fullWidth={true}
-                    />
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
+        <AppBar position="static" className="appbar">
+          <Toolbar className="app-toolbar">
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerToggle}
+              edge="start"
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap className={classes.title}>
+              {this.props.title}
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
           </Toolbar>
           {/* Todo - Implement Search Bar */}
         </AppBar>
@@ -74,4 +122,5 @@ class AppBarComp extends React.Component {
     );
   }
 }
-export default AppBarComp;
+
+export default withStyles(styles)(AppBarComp);
