f9d5a8b9e882eec6440bc5195fcef8fff370eb9e
LIT-Admin <aswin@littech.in>
2020-06-14 14:31:16 +0530

    env files

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
+require("dotenv").config();
 export const BRAND_NAME = "LIT"
-export const BASE_URL = "http://localhost:3002/api/v1/";
+export const BASE_URL = process.env.API_BASE_URL || "http://localhost:3002/api/v1/";
