7734e6b2d6543b11b768507844e4b5c7e7b84309
LIT-Admin <aswin@littech.in>
2020-06-22 22:27:43 +0530

    add support for env variable

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
-require("dotenv").config();
-export const BRAND_NAME = "HHYS"
-export const BASE_URL = process.env.API_BASE_URL || "http://localhost:3002/api/v1/";
+// require("dotenv").config();
+export const BRAND_NAME = process.env.REACT_APP_BRAND_NAME || "HHYS";
+export const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:3002/api/v1/hhys/";
+export const BRAND_HERO_IMAGE = process.env.BRAND_HERO_IMAGE || "";
+export const BRAND_LOGO_IMAGE = process.env.BRAND_LOGO_IMAGE || "";
