e3ed8d83d2deceb1421a1c62c0c098a497c62084
LIT-Admin <aswin@littech.in>
2020-06-29 12:43:47 +0530

    image upload set up - need upload api

--- src/components/common/imageupload.js
+++ src/components/common/imageupload.js
@@ -4,6 +4,7 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress"
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -19,6 +20,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ImageUploadComp(props){
   const classes = useStyles();
+  const[loader, setLoader] = React.useState(false)
+  const [image, setImage] = React.useState();
   const handleClose = () => {
     props.handleDialogClose();
   };
@@ -27,6 +30,13 @@ export default function ImageUploadComp(props){
     event.preventDefault();
     //pass src data back to parent
   }
+  const handleImageUpload = (event) => {
+    setLoader(true);
+    setImage(event.target.files[0]);
+    setTimeout(() => {
+      setLoader(false);
+    }, 2000);
+  };
   return (
     <Dialog
       open={props.open}
@@ -42,14 +52,20 @@ export default function ImageUploadComp(props){
             accept="image/*"
             className={classes.input}
             id="contained-button-file"
+            name="image"
             multiple
             type="file"
+            onChange={handleImageUpload}
           />
-          <label htmlFor="contained-button-file">
-            <Button variant="contained" color="primary" component="span">
-              Select Image
-            </Button>
-          </label>
+          {loader ? (
+            <CircularProgress />
+          ) : (
+            <label htmlFor="contained-button-file">
+              <Button variant="contained" color="primary" component="span">
+                {image ? "Replace Image" : "Select Image"}
+              </Button>
+            </label>
+          )}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
