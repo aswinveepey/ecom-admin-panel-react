5a1dcd684f15353c04ee6405e4f58d8ddf2c163a
LIT-Admin <aswin@littech.in>
2020-07-09 21:34:41 +0530

    changes to orderitem index

--- src/components/order/orderitemdetail.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-
-export default function OrderitemDetailComp(props){
-  return <React.Fragment></React.Fragment>;
-}
--- src/components/order/orderitemindex.js
+++ src/components/order/orderitemindex.js
@@ -5,9 +5,6 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import IconButton from "@material-ui/core/IconButton";
-import Button from "@material-ui/core/Button";
-import EditIcon from "@material-ui/icons/Edit";
 import OrderitemDetailComp from "./orderitemdetail";
 //Styles
 import { makeStyles } from "@material-ui/core/styles";
@@ -21,34 +18,13 @@ const useStyles = makeStyles((theme) => ({
 
 export default function OrderitemIndeComp(props) {
   const classes = useStyles();
-  const [orderitemDetailOpen, setOrderitemDetailOpen] = React.useState(false);
-  const [orderitemDetailData, setOrderitemDetailData] = React.useState([]);
 
-  //open Product Detail
-  const openOrderitemDetail = (orderitem, event) => {
-    event.preventDefault();
-    setOrderitemDetailData(orderitem);
-    setOrderitemDetailOpen(true);
-  };
-
-  const closeOrderitemDetail = () => {
-    setOrderitemDetailOpen(false);
-  };
   return (
     <React.Fragment>
       <TableContainer>
-        {/* <Button
-          color="primary"
-          variant="outlined"
-          aria-label="add"
-          onClick={openOrderitemDetail.bind(this, { order: props.order_id })}
-        >
-          Add Order 
-        </Button> */}
         <Table size="small" aria-label="orderitems" className={classes.table}>
           <TableHead>
             <TableRow>
-              <TableCell></TableCell>
               <TableCell className={classes.tablecell}>Name</TableCell>
               <TableCell>Status</TableCell>
               <TableCell>Booked Quantity</TableCell>
@@ -63,15 +39,6 @@ export default function OrderitemIndeComp(props) {
           <TableBody>
             {props.data?.map((orderitem) => (
               <TableRow key={orderitem.shortid}>
-                <TableCell>
-                  <IconButton
-                    size="small"
-                    onClick={openOrderitemDetail.bind(this, orderitem)}
-                    aria-label="orderitem detail"
-                  >
-                    <EditIcon />
-                  </IconButton>
-                </TableCell>
                 <TableCell className={classes.tablecell}>
                   {orderitem.sku?.product?.name + " " + orderitem.sku?.name}
                 </TableCell>
@@ -94,13 +61,6 @@ export default function OrderitemIndeComp(props) {
           </TableBody>
         </Table>
       </TableContainer>
-      {orderitemDetailOpen && (
-        <OrderitemDetailComp
-          open={orderitemDetailOpen}
-          handleClose={closeOrderitemDetail}
-          data={orderitemDetailData}
-        />
-      )}
     </React.Fragment>
   );
 }
