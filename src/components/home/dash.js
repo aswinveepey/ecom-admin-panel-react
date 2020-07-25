3eaad50b7b9de78121a482c9c5d0c3840242e155
LIT-Admin <aswin@littech.in>
2020-07-25 12:33:15 +0530

    optimize data dashboard - cleanup and refactor

--- src/components/home/dash.js
+++ src/components/home/dash.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import Grid from "@material-ui/core/Grid";
 import InfoBox from '../common/infobox'
-import DashChartComp from './dashchart'
+import GmvTimeSeriesComp from "./gmvtimeseries";
 
 import DataApi from "../../api/data";
 
 export default function DashComp(props) {
   const dataApi = new DataApi();
   const [customerData, setCustomerData] = React.useState()
-  const [currentGmv, setCurrentGmv] = React.useState();
+  const [todayGmv, setTodayGmv] = React.useState();
+  const [monthGmv, setMonthGmv] = React.useState();
   const [quarterGmv, setQuarterGmv] = React.useState();
-  const [monthlyGmv, setMonthlyGmv] = React.useState();
+  const [gmvTimeSeries, setGmvTimeSeries] = React.useState();
 
   React.useEffect(()=>{
     dataApi.getCustomerData().then((data) => data && setCustomerData(data[0]));
-    dataApi.getCurrentGmv().then((data) => data && setCurrentGmv(data[0]));
+    dataApi.getTodayGmv().then((data) => data && setTodayGmv(data[0]));
+    dataApi.getMonthGmv().then((data) => data && setMonthGmv(data[0]));
     dataApi.getQuarterGmv().then((data) => data && setQuarterGmv(data[0]));
-    dataApi.getMonthlyGmv().then((data) => data && setMonthlyGmv(data ));
+    dataApi.getGmvTimeSeries().then((data) => data && setGmvTimeSeries(data));
   },[props])
 
     return (
@@ -25,18 +27,24 @@ export default function DashComp(props) {
           <Grid item>
             <Grid container spacing={1}>
               <Grid item xs={12} sm={12} md={4} lg={4} key={1}>
-                <InfoBox title="No Of Customers" value={customerData?.count} />
+                <InfoBox
+                  title="No Of Customers"
+                  value={customerData?.count || 0}
+                />
               </Grid>
               <Grid item xs={12} sm={12} md={4} lg={4} key={2}>
-                <InfoBox title="MTD GMV" value={currentGmv?.total} />
+                <InfoBox title="Daily GMV" value={todayGmv?.total || 0} />
               </Grid>
               <Grid item xs={12} sm={12} md={4} lg={4} key={3}>
-                <InfoBox title="QTD GMV" value={quarterGmv?.total} />
+                <InfoBox title="MTD GMV" value={monthGmv?.total || 0} />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4} lg={4} key={4}>
+                <InfoBox title="QTD GMV" value={quarterGmv?.total || 0} />
               </Grid>
             </Grid>
           </Grid>
           <Grid item>
-            <DashChartComp monthlyGmv={monthlyGmv} />
+            <GmvTimeSeriesComp gmvTimeSeries={gmvTimeSeries} />
           </Grid>
         </Grid>
       </React.Fragment>
