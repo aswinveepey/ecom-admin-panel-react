ec912a6f4e859425fcc810851cdf0cb5d9665ed1
LIT-Admin <aswin@littech.in>
2020-07-28 16:12:19 +0530

    scaffold global to app components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Suspense, lazy } from "react";
 import './App.css';
 import LoaderComp from "./components/loader"
+import Scaffold from "./components/common/scaffold"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
 // import APIFeedbackProvider from "../src/providers/apifeedbackprovider";
 
 const Login = lazy(() => import("./components/login/login.js"));
@@ -14,21 +16,26 @@ const AdminComp = lazy(() => import("./components/admin/admin"));
 const NotFoundComp = lazy(() => import("./components/404"));
 
 export default function App() {
+  const state = useSelector((state) => state.userStateReducer);
   return (
-        <Router>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        <Scaffold title={"Hi, " + (state.user?.firstname || "")}>
           <Suspense fallback={<LoaderComp />}>
             <Switch>
-              <Route exact path="/" component={Login}></Route>
-              <Route exact path="/login" component={Login}></Route>
               <Route exact path="/home" component={Home}></Route>
               <Route exact path="/order" component={OrderComp}></Route>
-              <Route exact path="/user/:userid" component={UserComp}></Route>
+              {/* <Route exact path="/user/:userid" component={UserComp}></Route> */}
               <Route exact path="/customer" component={CustomerComp}></Route>
               <Route exact path="/catalog" component={CatalogComp}></Route>
               <Route exact path="/admin" component={AdminComp}></Route>
               <Route component={NotFoundComp}></Route>
             </Switch>
           </Suspense>
-        </Router>
+        </Scaffold>
+      </Switch>
+    </Router>
   );
 }
